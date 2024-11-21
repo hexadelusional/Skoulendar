31028eff86c2c5ac3babdb27e71d61db00a81d89
Adele <chamouxadele@gmail.com>
2024-11-21 11:47:55 +0800

    first skoulendar commit

---
+++ skoulendar/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>
+      Every class every day, <br>
+      Skoulendar leads the way !
+    </h1>
+    <Chad/>
+  </div>
+</template>
+
+
+<script setup>
+  import Chad from './components/Chad.vue'
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ skoulendar/src/components/Chad.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    🗿
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  div{
+    font-size: 50px;
+  }
+</style>
